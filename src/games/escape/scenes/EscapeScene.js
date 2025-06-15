561e08e7b036d7818e357783c9b035fffed43a35
mikanorm39 <mikanorm@outlook.jp>
2025-06-15 16:32:59 +0900

    initialize

---
+++ src/games/escape/scenes/EscapeScene.js
@@ -0,0 +1,13 @@
+import Phaser from "phaser";
+
+class EscapeScene extends Phaser.Scene {
+  constructor() {
+    super({ key: "EscapeScene" });
+  }
+
+  create() {
+    // ’EoƒV[ƒ“‚ÌÀ‘•
+  }
+}
+
+export default EscapeScene;
